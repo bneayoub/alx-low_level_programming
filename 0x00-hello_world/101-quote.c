697a2e3baf3a98d6f8e2571ee1e933c041be886c
bneayoub <bne.ayoub@gmail.com>
2023-03-16 00:09:47 -0700

    simplicity is hard to understand!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, quote, sizeof(quote)-1);
+write(STDERR_FILENO, quote, sizeof(quote) - 1);
 return (1);
 }
