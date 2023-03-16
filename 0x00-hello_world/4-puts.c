d60c7c88c17cf5d23d6859aa7952b4eb0ab3e014
bneayoub <bne.ayoub@gmail.com>
2023-03-15 23:41:14 -0700

    My first actual C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/* 
+ Main functions of type int, and outputs 0 that marks success
+ 
+ */
 int main(void) {
 
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 
 
 return 0;
