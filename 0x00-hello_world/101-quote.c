3f711d7da50906391cddaec92b1b62430b60656a
bneayoub <bne.ayoub@gmail.com>
2023-03-16 00:09:01 -0700

    simplicity is hard to understand!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, quote, sizeof(quote)-1);
+return (1);
+}
