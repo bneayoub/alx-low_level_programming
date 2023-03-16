7aa29df40815e9fb3dd8f86b6485ee9b8ac2524a
bneayoub <bne.ayoub@gmail.com>
2023-03-16 00:00:41 -0700

    size of different types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char Char;
 long int LongInt;
 float Float;
 long long int LongLongInt;
-printf("Size of a char: %lu byte(s)\n", sizeof(Char));
-printf("Size of an int: %lu byte(s)\n", sizeof(Int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(LongInt));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(LongLongInt));
-printf("Size of a float: %lu byte(s)\n", sizeof(Float));
+printf("Size of a char: %d byte(s)\n", sizeof(Char));
+printf("Size of an int: %d byte(s)\n", sizeof(Int));
+printf("Size of a long int: %d byte(s)\n", sizeof(LongInt));
+printf("Size of a long long int: %d byte(s)\n", sizeof(LongLongInt));
+printf("Size of a float: %d byte(s)\n", sizeof(Float));
 return (0);
 }
