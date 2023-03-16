4394bd26e611fdc68b301bf01bde54fb7bb96302
bneayoub <bne.ayoub@gmail.com>
2023-03-15 23:50:25 -0700

    My first actual C file final, too much ** checker tests

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*
-*main - function of type int
-*return: 0
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
