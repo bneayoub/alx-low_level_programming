2047e77506af78d4a7716d3f0f29673490600dfc
bneayoub <bne.ayoub@gmail.com>
2023-03-17 04:59:10 -0700

    all single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,11 @@ int number;
 for (number = 0; number <= 9; number++)
 {
 	putchar((number % 10) + '0');
-
 	if (number == 9)
 		continue;
 	putchar(',');
 	putchar(' ');
 }
 putchar('\n');
-
 return (0);
 }
