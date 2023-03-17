8a9f556a4466f6b7b1fb5e3001eada34125dddcb
bneayoub <bne.ayoub@gmail.com>
2023-03-17 03:02:42 -0700

    correct sign script

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* My coder starts here */
+	if n>0
+	{
+		printf("%d is positive\n",n)
+	}
+	if n<0
+        {
+                printf("%d is negative\n",n)
+        }
+	if n==0
+        {
+                printf("%d is zero\n",n)
+        }
+	return (0);
+}
