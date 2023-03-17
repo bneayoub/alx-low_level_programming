2730886b5de1c411ba699870cd86d69d99de1c60
bneayoub <bne.ayoub@gmail.com>
2023-03-17 05:36:04 -0700

    all two two-digits numbers combos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combos of two two-digits.
+ *
+ * Return: 0
+*/
+int main(void)
+{
+int number1 = 0, number2 = 1;
+
+while (number1 < 100)
+{
+	while (number2 < 100)
+	{
+		putchar(number1 / 10 + '0');
+		putchar(number1 % 10 + '0');
+		putchar(' ');
+		putchar(number2 / 10 + '0');
+		putchar(number2 % 10 + '0');
+		if (number1 != 98 || number2 != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number2++;
+	}
+	number1++;
+	number2 = number1 + 1;
+}
+return (0);
+}
