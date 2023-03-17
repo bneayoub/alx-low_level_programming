4861b4743ab47fb1a29f262ec99e91947f0401f6
bneayoub <bne.ayoub@gmail.com>
2023-03-17 04:57:45 -0700

    all single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - all single digits numberbers separated with , 
+ *
+ * Return: 0
+*/
+int main(void)
+{
+int number;
+
+for (number = 0; number <= 9; number++)
+{
+	putchar((number % 10) + '0');
+
+	if (number == 9)
+		continue;
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+
+return (0);
+}
