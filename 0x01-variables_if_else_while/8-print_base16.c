4299f7fa8c02e8c0629c8d3e1dfb6d941ec4779a
bneayoub <bne.ayoub@gmail.com>
2023-03-17 04:52:59 -0700

    all hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - all hexadecimal numbers
+ *
+ * Return: 0
+*/
+
+ main(void)
+{
+char number_char = '0';
+char alphabet = 'a';
+
+while (number_char <= '9')
+{
+	putchar(number_char);
+	number_char++;
+}
+while (alphabet < 'g')
+{
+	putchar(alphabet);
+	alphabet++;
+}
+putchar('\n');
+
+return (0);
+}
