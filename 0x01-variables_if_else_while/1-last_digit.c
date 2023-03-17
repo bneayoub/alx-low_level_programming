b77fa86eaac2337e888aa52d4e23695b7931067d
bneayoub <bne.ayoub@gmail.com>
2023-03-17 03:37:31 -0700

    last digit evaluation

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ int main(void)
 	}
 	if (n % 10 == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n , n % 10);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
