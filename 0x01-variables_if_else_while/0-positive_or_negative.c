d6e46e74ba6504652c3341840ded899150d8894b
bneayoub <bne.ayoub@gmail.com>
2023-03-17 03:13:04 -0700

    1st script fixed after ansi and betty standards

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* My coder starts here */
-	if (n>0)
+	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+	printf("%d is positive\n" , n);
 	}
-	if (n<0)
-        {
-                printf("%d is negative\n",n);
-        }
-	if (n==0)
-        {
-                printf("%d is zero\n",n);
-        }
+	if (n < 0)
+	{
+	printf("%d is negative\n" , n)
+	}
+	if (n == 0)
+	{
+	printf("%d is zero\n" , n);
+	}
 	return (0);
 }
