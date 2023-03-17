85f3d9e742ba75145ebb9d741863c61af0f3273d
bneayoub <bne.ayoub@gmail.com>
2023-03-17 04:46:04 -0700

    all alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - all alphabets in reverse.
+ *
+ * Return: 0
+*/
+int main(void)
+{int alphabet = 'z';
+
+while (alphabet >= 'a')
+{
+    putchar(alphabet);
+    alphabet--;
+}
+
+putchar('\n');
+return (0);
+}
