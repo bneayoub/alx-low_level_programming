1b15d809e70e6063c8e097aadd0e83943ff74f9c
bneayoub <bne.ayoub@gmail.com>
2023-03-17 03:09:14 -0700

    1st script fixed after ansi and betty standards

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 	/* My coder starts here */
 	if (n>0)
 	{
-		printf("%d is positive\n",n)
+		printf("%d is positive\n",n);
 	}
 	if (n<0)
         {
-                printf("%d is negative\n",n)
+                printf("%d is negative\n",n);
         }
 	if (n==0)
         {
-                printf("%d is zero\n",n)
+                printf("%d is zero\n",n);
         }
 	return (0);
 }
