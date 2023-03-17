69fe58b05e70f33a77441d3868e6cf6512a23165
bneayoub <bne.ayoub@gmail.com>
2023-03-17 04:24:28 -0700

    all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - all digits from 0 to 9
+ *
+ * Return: 0
+*/
+int main(void)
+{
+int number=0;
+
+while (number <= 9)
+{
+	printf("%d", number);
+    number++;
+}
+printf("\n");
+return (0);
+}
