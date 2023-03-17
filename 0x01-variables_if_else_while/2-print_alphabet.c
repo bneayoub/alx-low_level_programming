e05530a608bfe9c8f13094025b1819aa14819725
bneayoub <bne.ayoub@gmail.com>
2023-03-17 04:00:27 -0700

    all alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - print all alphabets
+ *
+ * Return: 0
+*/
+int main(void)
+{
+char alphabet='a';
+while (alphabet <= 'z')
+	puts(alphabet);
+	alphabet++;
+puts('\n')
+return (0)
+}
