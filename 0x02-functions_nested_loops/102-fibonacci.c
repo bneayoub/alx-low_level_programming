bc6fb37c2304979030da3898c3d86e1930268507
bneayoub <bne.ayoub@gmail.com>
2023-03-21 22:54:48 +0000

    Fibonacci version 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - entry point of the program
+  *
+  * Return: always 0
+  */
+int main(void)
+{
+	unsigned long fibo1 = 1;
+	unsigned long fibo2 = 2;
+	int i = 0;
+
+	while (i < 50)
+	{
+		printf("%lu", fibo1);
+		if (i < 49)
+		{
+			fibo2 += fibo1;
+			fibo1 = fibo2 - fibo1;
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
