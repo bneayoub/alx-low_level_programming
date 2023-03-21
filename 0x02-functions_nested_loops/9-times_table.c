e84d489d597871625f690dc157ccd7bb4c9f640e
bneayoub <bne.ayoub@gmail.com>
2023-03-21 19:35:00 +0000

    9 table times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * times_table - aboslute value
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i = 0;
+	int j = 0;
+	char a = ',';
+	char b = ' ';
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d", i * j);
+			fflush(stdout);
+			if (i * j == (i * j) % 10 && j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -6,6 +6,5 @@
  */
 int _putchar(char s)
 {
-	write(1, &s, 1);
-	return (0);
+	return (write(1, &s, 1));
 }
