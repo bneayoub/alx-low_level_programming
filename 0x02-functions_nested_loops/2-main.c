11b6b54bff13b1f4f086f8207a7a465adf15671b
bneayoub <bne.ayoub@gmail.com>
2023-03-21 06:57:00 +0000

    10x printiiing

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+void print_alphabet_x10(void);
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+int _putchar(char c);
+/**
+ * main - _putchar alphabets
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int j = 0;
+	char character = 'a';
+	while (j < 10)
+	{
+		while (character <= 'z')
+		{
+			_putchar(character);
+			character++;
+		}
+		_putchar('\n');
+		j++;
+		character = 'a';
+	}
+}
