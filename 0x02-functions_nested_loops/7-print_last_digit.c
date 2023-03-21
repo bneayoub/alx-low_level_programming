61a30fccfc02ad42eaf62b176e4fa8a41316eca6
bneayoub <bne.ayoub@gmail.com>
2023-03-21 17:29:53 +0000

    last digit, betty fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,12 @@
 /**
  * _abs - aboslute value
  * @n: the number to calculates
- * Description: Longer description of the function)?
-(* section header: Section description)*
  * Return: 0
  */
 int print_last_digit(int n)
 {
 	int a = n % 10;
+
 	if (a < 0)
 	{
 		a = -1 * a;
