c959fc738187fcbac9870ec77daea73970ecbae3
bneayoub <bne.ayoub@gmail.com>
2023-03-21 17:20:09 +0000

    last_digit first try

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - aboslute value
+ * @n: the number to calculates
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int a = n % 10;
+	_putchar(a);
+	return (0);
+}
