b56b7ff6854d50303e9f121d4a6855cb4c2e606e
bneayoub <bne.ayoub@gmail.com>
2023-03-21 18:22:28 +0000

    taabs added

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,21 +5,21 @@
  */
 void jack_bauer(void)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    for (i = 0; i <= 23; i++)
-    {
-        int s = i / 10;
-        for (j = 0; j <= 59; j++)
-        {
-            int k = j / 10;
-            _putchar(s % 10 + '0');
-            _putchar(i % 10 + '0');
-            _putchar(':');
-            _putchar(k % 10 + '0');
-            _putchar(j % 10 + '0');
-            _putchar('\n');
-        }
-    }
+	for (i = 0; i <= 23; i++)
+	{
+		int s = i / 10;
+		for (j = 0; j <= 59; j++)
+		{
+			int k = j / 10;
+			_putchar(s % 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(k % 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
