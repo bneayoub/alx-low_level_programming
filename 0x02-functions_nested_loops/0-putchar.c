2d30a24eda98610cf0c63541cc0e47dea7badffc
bneayoub <bne.ayoub@gmail.com>
2023-03-21 06:10:05 +0000

    Betty putchar married

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
 int _putchar(char c);
-
+/**
+ * main - _putchar
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,9 +3,9 @@
  * main - _putchar
  *
  * Return: 0
-*/
+ */
 int _putchar(char s)
 {
-    write(1, &s, 1);
-    return (0);
+	write(1, &s, 1);
+	return (0);
 }
