1bfd16609aa9982810b40ca53f8ad2a11ce15bbe
bneayoub <bne.ayoub@gmail.com>
2023-03-21 16:05:04 +0000

    sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - prints sign of it.
+ *
+ * Return: 0,1, or -1.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
