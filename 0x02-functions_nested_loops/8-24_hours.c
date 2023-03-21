3af76a7017dc377a812adfe8285073e6bb631b0f
bneayoub <bne.ayoub@gmail.com>
2023-03-21 18:21:32 +0000

    jack bauer tik tok

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - aboslute value
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; i <= 23; i++)
+    {
+        int s = i / 10;
+        for (j = 0; j <= 59; j++)
+        {
+            int k = j / 10;
+            _putchar(s % 10 + '0');
+            _putchar(i % 10 + '0');
+            _putchar(':');
+            _putchar(k % 10 + '0');
+            _putchar(j % 10 + '0');
+            _putchar('\n');
+        }
+    }
+}
