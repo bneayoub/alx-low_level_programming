14175a80ecd392fedeba053b1fa5cdb086bff8b3
bneayoub <bne.ayoub@gmail.com>
2023-03-22 10:40:31 +0000

    positive or negative

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - prints the sign of the number
+ * positive_or_negative - prints the sign of the number
  *
- * Return: 0
-*/
+ * Return: void
+ */
 void positive_or_negative(int n)
 {
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	if (n < 0)
 	{
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 }
