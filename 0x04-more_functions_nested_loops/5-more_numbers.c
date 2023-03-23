08b989c9d2da02f6591269ff65bc291783a6d751
bneayoub <bne.ayoub@gmail.com>
2023-03-23 06:22:23 +0000

    print 0 ...14 x10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0..14 x10
+ * Return: voide
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (j < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i < 10)
+			{
+				_putchar(i + '0');
+			}
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+		}
+		j++;
+		_putchar('\n');
+	}
+}
