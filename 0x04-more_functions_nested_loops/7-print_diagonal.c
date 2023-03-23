5b3701c917fc93c77a7d88a33795236e3edaa974
bneayoub <bne.ayoub@gmail.com>
2023-03-23 13:08:42 +0000

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
