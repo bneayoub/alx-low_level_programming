03114618fba6d5b348e119cb50af3af2b1fa6eca
bneayoub <bne.ayoub@gmail.com>
2023-03-23 14:17:22 +0000

    More numbers fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,14 +15,9 @@ void more_numbers(void)
 			if (i >= 10)
 			{
 				_putchar(i / 10 + '0');
-				i = i % 10;
 			}
-			else
-			{
-				_putchar(i%10 + '0');
-				i=i+10;
-			}
-			
+
+			_putchar(i % 10 + '0');
 		}
 		j++;
 		_putchar('\n');
