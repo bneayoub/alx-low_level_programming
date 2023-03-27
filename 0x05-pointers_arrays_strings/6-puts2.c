461464e516079bf201a8e6f48ce1acb63a0c74b7
bneayoub <bne.ayoub@gmail.com>
2023-03-27 18:33:46 +0000

    str even ids only

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,11 @@ void puts2(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		_putchar(*(str + i));
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		i++;
 	}
 	_putchar('\n');
 }
