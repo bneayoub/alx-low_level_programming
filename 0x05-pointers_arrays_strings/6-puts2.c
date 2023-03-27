9c01566f405c55d6370138f8587fec68a4dad6e6
bneayoub <bne.ayoub@gmail.com>
2023-03-27 18:29:05 +0000

    str even indexes only

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str+i)!='\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(*(str+i));
-		i+=2;
+		_putchar(*(str + i));
+		i += 2;
 	}
 	_putchar('\n');
 }
