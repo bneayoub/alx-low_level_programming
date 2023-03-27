e5059bdd8a42fbc0e2fae97ce88a90eb24b2c940
bneayoub <bne.ayoub@gmail.com>
2023-03-27 18:06:27 +0000

    string reversed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - check the code
+ * @s: pointer to string
+ * Return: string in reverse \n.
+ */
+void print_rev(char *s)
+{
+	int len;
+	len = _strlen(s);
+
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
+	_putchar('\n');
+}
