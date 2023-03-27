c3e8049b11bcd2d46b621552dfb0a444054b4189
bneayoub <bne.ayoub@gmail.com>
2023-03-27 18:48:37 +0000

    array elems

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	if (n >= 0)
+	for (i = 0; i < n; i++)
 	{
-		while (i < n)
-		{
-			if (i + 1 < n)
-				printf("%d, ", a[i]);
-			else
-				printf("%d", a[i]);
-			i++;
-		}
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
 	}
 	printf("\n");
 }
