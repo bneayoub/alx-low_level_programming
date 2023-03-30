b40465b8d514bffefd1336c0fbd13113b9e31f77
bneayoub <bne.ayoub@gmail.com>
2023-03-30 15:32:38 +0000

    copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,13 +7,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i =0;
+	int i = 0;
 
-	while (i<n && src[i]!='\0')
+	while (i < n && src[i] != '\0')
 	{
-		dest[i]=src[i];
+		dest[i] = src[i];
 		i++;
 	}
 
-	return dest;
+	return (dest);
 }
