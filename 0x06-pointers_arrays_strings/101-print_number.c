9064c1591d0a2dcd6de89d64ad11ce1ba2570b99
bneayoub <bne.ayoub@gmail.com>
2023-03-30 17:10:34 +0000

    print number using _putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: string to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	int div = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / div >= 10)
+	{
+		div *= 10;
+	}
+
+	while (div != 0)
+	{
+		int digit = n / div;
+
+		_putchar(digit + '0');
+		n %= div;
+		div /= 10;
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+int _putchar(char s);
