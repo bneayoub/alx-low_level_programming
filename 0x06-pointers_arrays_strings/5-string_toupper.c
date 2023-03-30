bfd597e9cfc583a15eacfefb0b7ae17a9524b73f
bneayoub <bne.ayoub@gmail.com>
2023-03-30 16:29:49 +0000

    To upper !

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * *string_toupper - changes all lower case to upper
+ * @str: string to toupper!
+ * Return: all upper
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+
+	return str;
+}
