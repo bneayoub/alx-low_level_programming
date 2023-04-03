50814003903aa0706f7459854aa4687b0b311197
bneayoub <bne.ayoub@gmail.com>
2023-04-03 19:32:24 +0000

    lets see now

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,20 +10,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i=0;
 
-	if (*s == c)
+	while (s[i])
 	{
-		return (s);
-	}
-
-	for (i = 0; s[i] != '\0' ; ++i)
-	{
-		if (*(s + i) == c)
+		if (s[i] == c)
 		{
 			return (s + i);
 		}
+		i++;
 	}
-
 	return (NULL);
 }
