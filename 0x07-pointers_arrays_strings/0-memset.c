58cb85c3f5bb467a0842654852d35508f4d6edec
bneayoub <bne.ayoub@gmail.com>
2023-04-03 16:41:21 +0000

    _memset description added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,13 @@
+/**
+ * _memset - copies the character b to the first n character
+ * to the string pointed to it
+ *
+ * @s: first number to add
+ * @b: second number to add
+ * @n: buffer to store result
+ *
+ * Return: pointer to result buffer, or 0 if result can't be stored
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
