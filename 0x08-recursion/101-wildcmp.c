ebf049cb0e7f4572480ce32e4c20a38b0204a556
bneayoub <bne.ayoub@gmail.com>
2023-04-06 02:54:08 +0000

    wild comparing with recursion, and with no loop

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,11 +22,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s1 == '\0')
 		{
-			while (*s2 == '*')
-			{
-				s2++;
-			}
-			return (*s2 == '\0');
+			return (wildcmp(s1, s2 + 1));
 		}
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
