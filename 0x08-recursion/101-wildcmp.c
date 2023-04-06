fec065b7244e4dc7618b3a7be52a243d95622501
bneayoub <bne.ayoub@gmail.com>
2023-04-06 02:46:38 +0000

    wild comparing with recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,16 +14,16 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-
-	if (*s1 == *s2)
+	else if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-
-	if (*s2 == '*')
+	else if (*s2 == '*')
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
-
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
