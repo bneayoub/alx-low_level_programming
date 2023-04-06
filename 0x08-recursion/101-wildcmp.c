044978cdb53199d1b45af610750656b33684c074
bneayoub <bne.ayoub@gmail.com>
2023-04-06 02:19:25 +0000

    wild comparing using recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+/**
+ * wildcmp - helps comparing 2 strings
+ * using recursion
+ *
+ * @s1: string 2 to compare
+ * @s2: string 2 to compare
+ *
+ * Return: 0 if *s1 and *s2 are different
+ * 1 if *s1 and *s2 can be considered identical
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+
+	return (0);
+}
