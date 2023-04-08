e94174ef1d78657d151699959ea5663ffaaf2ba8
bneayoub <bne.ayoub@gmail.com>
2023-04-08 01:25:41 +0000

    test push to see where the error comes from

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,7 +23,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
 
 
 #endif
