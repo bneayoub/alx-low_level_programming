efbb48bf6c6afcdd65737fa169dc31f93d1eed27
bneayoub <bne.ayoub@gmail.com>
2023-04-08 01:46:56 +0000

    main.h problem confirmed, what abt now ?

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +25,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
