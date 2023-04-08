a435039e2aa85d3beb971ccfe4a29264f32b6954
bneayoub <bne.ayoub@gmail.com>
2023-04-08 22:57:09 +0000

    number of arguments passed, name excluded

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
- * main - prints the program name
+ * main - prints the number of arguments passed
+ * to the program
  *
  * @argc: the number of arguments passed to the program
  * @argv: pointer to an array of chars represents
  * arguments passed to the program
  *
- * Return: int and prints the program name
- * followed by a new line
+ * Return: int and prints the number of arguments
+ * passed to the program
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
