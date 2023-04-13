095fe811b0ccc52e2a0c6bf5fabb5fa588b8f26f
bneayoub <bne.ayoub@gmail.com>
2023-04-13 12:46:18 +0000

    calloc using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *tmp;
 	unsigned int i;
 
-	if (nmemb == 0 || array_malloc == NULL)
+	if (nmemb == 0 || array_malloc == NULL || size == 0)
 		return (NULL);
 
 	tmp = array_malloc;
