cc7cefa77b8e3045a553eb5e72402f8cfc15aecf
bneayoub <bne.ayoub@gmail.com>
2023-04-14 02:40:25 +0000

    multiplication using traditional way

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,108 +1,169 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+
+char *create_array_x(int size);
+char *skip_leading_zeroes(char *str);
+void multiply_strings(char *prod, char *mult, int digit, int zeroes);
+void add_string_numbers(char *final_prod, char *next_prod, int next_len);
 
 /**
- * check_digits - checks if a string contains only digits
- * @str: the string to check
+ * create_array_x - Allocates memory for an array
+ * of chars, initializes each element
+ * with the character 'x', and appends a null terminator at the end.
  *
- * Return: 1 if the string contains only digits, 0 otherwise
+ * @size: The size of the array to be created and initialized.
+ *
+ * Return: A pointer to the newly created and initialized array.
  */
-int check_digits(const char *str)
+char *create_array_x(int size)
 {
+	char *array;
 	int i;
 
-	for (i = 0; str[i]; i++)
-	{
-		if (!isdigit(str[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		exit(98);
+
+	for (i = 0; i < (size - 1); i++)
+		array[i] = 'x';
+
+	array[i] = '\0';
+
+	return (array);
 }
 
 /**
- * add_strings - adds two strings representing numbers
- * @num1: first number as a string
- * @num2: second number as a string
+ * skip_leading_zeroes - Advances the pointer through
+ * a numeric string, skipping
+ * any leading zeroes, until a non-zero character is reached.
  *
- * Return: the sum of the two numbers as a string (must be freed by caller)
+ * @str: The string of numbers to be processed.
+ *
+ * Return: A pointer to the first non-zero character in the string.
  */
-char *add_strings(char *num1, char *num2)
+char *skip_leading_zeroes(char *str)
 {
-	int len1 = strlen(num1), len2 = strlen(num2);
-	int max_len = len1 > len2 ? len1 : len2;
-	char *result = calloc(max_len + 2, sizeof(char));
-	int carry = 0;
-	int i;
+	while (*str && *str == '0')
+		str++;
 
-	for (i = 0; i < max_len; i++)
+	return (str);
+}
+
+/**
+ * char_to_int - Converts a single digit character to
+ * its corresponding integer value.
+ *
+ * @c: The character to be converted.
+ *
+ * Return: The integer representation of the digit character.
+ */
+int char_to_int(char c)
+{
+	int digit = c - '0';
+
+	if (digit < 0 || digit > 9)
 	{
-		int n1 = i < len1 ? num1[len1 - 1 - i] - '0' : 0;
-		int n2 = i < len2 ? num2[len2 - 1 - i] - '0' : 0;
-		int current_sum = n1 + n2 + carry;
-
-		result[max_len - i] = (current_sum % 10) + '0';
-		carry = current_sum / 10;
+		printf("Error\n");
+		exit(98);
 	}
 
-	if (carry)
-	{
-		result[0] = carry + '0';
-	}
-	else
-	{
-		memmove(result, result + 1, max_len + 1);
-	}
-
-	return (result);
+	return (digit);
 }
 
 /**
  * multiply_strings - multiplies two strings representing numbers
- * @num1: first number as a string
- * @num2: second number as a string
+ * @prod: buffer of the result
+ * @mult: The string of numbers.
+ * @digit: The single digit.
+ * @zeroes: The necessary number of leading zeroes.
  *
- * Return: the product of the two numbers as a string (must be freed by caller)
+ * Return : multiplication
  */
-char *multiply_strings(const char *num1, const char *num2)
+void multiply_strings(char *prod, char *mult, int digit, int zeroes)
 {
-	int len1 = strlen(num1), len2 = strlen(num2);
-	char *result = malloc((len1 + len2 + 1) * sizeof(char));
-	int i, j, k;
+	int mult_len, num, tens = 0;
 
-	for (k = 0; k < len1 + len2; k++)
+	mult_len = strlen(mult) - 1;
+	mult += mult_len;
+
+	while (*prod)
 	{
-		result[k] = '0';
+		*prod = 'x';
+		prod++;
 	}
-	result[len1 + len2] = '\0';
 
-	for (i = len2 - 1; i >= 0; i--)
+	prod--;
+
+	while (zeroes--)
 	{
-		int carry = 0;
+		*prod = '0';
+		prod--;
+	}
 
-		for (j = len1 - 1; j >= 0; j--)
+	for (; mult_len >= 0; mult_len--, mult--, prod--)
+	{
+		if (*mult < '0' || *mult > '9')
 		{
-			int product = (num1[j] - '0') * (num2[i] - '0') + carry;
-			int current_sum = result[i + j + 1] - '0' + product;
+			printf("Error\n");
+			exit(98);
+		}
 
-			result[i + j + 1] = (current_sum % 10) + '0';
-			carry = current_sum / 10;
-		}
-		if (carry)
-		{
-			result[i] = (result[i] - '0' + carry) % 10 + '0';
-		}
+		num = (*mult - '0') * digit;
+		num += tens;
+		*prod = (num % 10) + '0';
+		tens = num / 10;
 	}
 
-	if (result[0] == '0')
+	if (tens)
+		*prod = (tens % 10) + '0';
+}
+
+/**
+ * add_string_numbers - Adds the numeric values represented by two strings.
+ *
+ * @final_prod: The buffer storing the current sum of the numbers.
+ * @next_prod: The next number to be added, represented as a string.
+ * @next_len: The length of the next_prod string.
+ *
+ * Return : The function assumes both input strings contain only digits
+ * and updates the final_prod buffer with the new sum.
+ */
+void add_string_numbers(char *final_prod, char *next_prod, int next_len)
+{
+	int num, tens = 0;
+
+	while (*(final_prod + 1))
+		final_prod++;
+
+	while (*(next_prod + 1))
+		next_prod++;
+
+	for (; *final_prod != 'x'; final_prod--)
 	{
-		memmove(result, result + 1, len1 + len2);
+		num = (*final_prod - '0') + (*next_prod - '0');
+		num += tens;
+		*final_prod = (num % 10) + '0';
+		tens = num / 10;
+
+		next_prod--;
+		next_len--;
 	}
 
-	return (result);
+	for (; next_len >= 0 && *next_prod != 'x'; next_len--)
+	{
+		num = (*next_prod - '0');
+		num += tens;
+		*final_prod = (num % 10) + '0';
+		tens = num / 10;
+
+		final_prod--;
+		next_prod--;
+	}
+
+	if (tens)
+		*final_prod = (tens % 10) + '0';
 }
 
 /**
@@ -114,7 +175,8 @@ char *multiply_strings(const char *num1, const char *num2)
  */
 int main(int argc, char *argv[])
 {
-	char *result;
+	char *final_prod, *next_prod;
+	int size, index, digit, zeroes = 0;
 
 	if (argc != 3)
 	{
@@ -122,15 +184,35 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (!check_digits(argv[1]) || !check_digits(argv[2]))
+	if (*(argv[1]) == '0')
+		argv[1] = skip_leading_zeroes(argv[1]);
+	if (*(argv[2]) == '0')
+		argv[2] = skip_leading_zeroes(argv[2]);
+	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
 	{
-		printf("Error\n");
-		exit(98);
+		printf("0\n");
+		return (0);
 	}
 
-	result = multiply_strings(argv[1], argv[2]);
-	printf("%s\n", result);
-	free(result);
+	size = strlen(argv[1]) + strlen(argv[2]);
+	final_prod = create_array_x(size + 1);
+	next_prod = create_array_x(size + 1);
+
+	for (index = strlen(argv[2]) - 1; index >= 0; index--)
+	{
+		digit = char_to_int(*(argv[2] + index));
+		multiply_strings(next_prod, argv[1], digit, zeroes++);
+		add_string_numbers(final_prod, next_prod, size - 1);
+	}
+	for (index = 0; final_prod[index]; index++)
+	{
+		if (final_prod[index] != 'x')
+			putchar(final_prod[index]);
+	}
+	putchar('\n');
+
+	free(next_prod);
+	free(final_prod);
 
 	return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,10 +7,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int check_digits(const char *str);
-char *add_strings(char *num1, char *num2);
-char *multiply_strings(const char *num1, const char *num2);
-int main(int argc, char *argv[]);
-
 
 #endif
