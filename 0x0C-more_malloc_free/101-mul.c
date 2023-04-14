43c69e95e0e940fb6182e1cc37c6f92c29b2a64f
bneayoub <bne.ayoub@gmail.com>
2023-04-14 06:25:39 +0000

    multiplication using traditional way final version

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -46,44 +46,44 @@ int _isdigit(char *s)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	char *str1, *str2;
+	int len_str1, len_str2, len, i, carry, digit1, digit2, *array_mul, a = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !_isdigit(s1) || !_isdigit(s2))
+	str1 = argv[1], str2 = argv[2];
+	if (argc != 3 || !_isdigit(str1) || !_isdigit(str2))
 		error();
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
+	len_str1 = strlen(str1);
+	len_str2 = strlen(str2);
+	len = len_str1 + len_str2 + 1;
+	array_mul = malloc(sizeof(int) * len);
+	if (!array_mul)
 		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (i = 0; i <= len_str1 + len_str2; i++)
+		array_mul[i] = 0;
+	for (len_str1 = len_str1 - 1; len_str1 >= 0; len_str1--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = str1[len_str1] - '0';
 		carry = 0;
-		for (len2 = strlen(s2) - 1; len2 >= 0; len2--)
+		for (len_str2 = strlen(str2) - 1; len_str2 >= 0; len_str2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			digit2 = str2[len_str2] - '0';
+			carry += array_mul[len_str1 + len_str2 + 1] + (digit1 * digit2);
+			array_mul[len_str1 + len_str2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			array_mul[len_str1 + len_str2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
-		if (result[i])
+		if (array_mul[i])
 			a = 1;
 		if (a)
-			_putchar(result[i] + '0');
+			_putchar(array_mul[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
-	free(result);
+	free(array_mul);
 	return (0);
 }
