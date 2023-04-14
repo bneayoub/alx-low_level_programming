2d4a597b1e803e01ccfc4e8da5cfd8e5cb55154d
bneayoub <bne.ayoub@gmail.com>
2023-04-14 00:11:56 +0000

    3d Task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,6 @@ int _putchar(char s);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif
