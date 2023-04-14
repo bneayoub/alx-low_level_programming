04f901674ea000d1ee2e18c7cb53d363af4c272a
bneayoub <bne.ayoub@gmail.com>
2023-04-14 04:39:52 +0000

    multiplication using traditional way

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
+ * error - handles errors for main
  *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * Return : exit with 98, and prints Error
  */
-int is_digit(char *s)
+void error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * _isdigit - verifies if a given string consists
+ * exclusively of digit characters
+ *
+ * @s: the string to be checked
+ *
+ * Return: 1 if all characters in the string are digits,
+ * 0 if any non-digit character is present
+ */
+int _isdigit(char *s)
 {
 	int i = 0;
 
@@ -22,37 +37,12 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
+ * main - entry point of the program
  *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: always 0 (Success)
+ * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
@@ -60,10 +50,10 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	if (argc != 3 || !_isdigit(s1) || !_isdigit(s2))
+		error();
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
@@ -74,7 +64,7 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (len2 = strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
