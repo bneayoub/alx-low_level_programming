7a466a5a4f908f8ebb26d3681b8ace72f177069f
bneayoub <bne.ayoub@gmail.com>
2023-04-17 14:39:33 +0000

    Poppy dooggy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/*
+ * File: dog.h
+ * Desciption: Header defining struct dog.
+ */
+
+/**
+ * struct dog - A new type defining a dog.
+ *
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
