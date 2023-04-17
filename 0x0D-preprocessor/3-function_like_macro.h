89de13ef6fd778d93b52150b34f8abc34f3d199e
bneayoub <bne.ayoub@gmail.com>
2023-04-17 12:54:47 +0000

    Function like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/*
+ * File: function_like_macro.h
+ * Desciption : Header defining a function like macro ABS
+ */
+
+#define ABS(x) ((x) > 0 ? (x) : (-x))
+
+#endif
