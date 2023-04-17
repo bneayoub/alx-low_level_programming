e96cdb0e28ab0b9ecb1d1bffeeaf202489ddcf15
bneayoub <bne.ayoub@gmail.com>
2023-04-17 16:17:01 +0000

    FREE dogs 4life

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - Frees the memory allocated to dogs
+ *
+ * @d: dog to be freed.
+ * 
+ * Return : void, dog freed
+ */
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+	free(d->owner);
+	free(d->name);
+	free(d);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,5 +25,6 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
