052a259894b2245972ac9fa8bf62c69c95593b26
bneayoub <bne.ayoub@gmail.com>
2023-04-17 12:48:55 +0000

    Description added

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,9 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
+/*
+ * File: object_like_macro.h
+ * Desciption: Header defining SIZE=1024
+ */
 
 #define SIZE 1024
 
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,9 @@
 #ifndef PI_H
 #define PI_H
+/*
+ * File: 1-pi.h
+ * Desciption: Header defining PI number
+ */
 
 #define PI 3.14159265359
 
