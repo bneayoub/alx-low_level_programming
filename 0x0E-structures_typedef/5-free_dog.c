095a867b6caaf98c8b01292a111a0f5ec91c85d0
bneayoub <bne.ayoub@gmail.com>
2023-04-17 16:19:52 +0000

    FREE Dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,15 +4,15 @@
  * free_dog - Frees the memory allocated to dogs
  *
  * @d: dog to be freed.
- * 
+ *
  * Return : void, dog freed
  */
 void free_dog(dog_t *d)
 {
-	if(d)
+	if (d)
 	{
-	free(d->owner);
-	free(d->name);
-	free(d);
+		free(d->owner);
+		free(d->name);
+		free(d);
 	}
 }
