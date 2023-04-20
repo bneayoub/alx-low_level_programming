dc8e6058f37d130d11c6e3c5b8b81d3cfd9409e2
bneayoub <bne.ayoub@gmail.com>
2023-04-20 18:18:43 +0000

    sum of args

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,12 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (n == 0)
-		return (0);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
+	va_end(args);
 
 	return (sum);
 }
