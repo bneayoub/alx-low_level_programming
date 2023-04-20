07fd8a5c1e08fee156a1597c388ee02a150dff85
bneayoub <bne.ayoub@gmail.com>
2023-04-20 19:13:48 +0000

    fixed print all- removed ifs

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,31 +14,38 @@ void print_all(const char *const format, ...)
 	int i = 0;
 	const char *str;
 	char current;
+	int should_print_comma;
 
 	va_start(args, format);
 
 	while (format[i])
 	{
 		current = format[i];
-		if (current == 'c')
-			printf("%c", (char)va_arg(args, int));
-		else if (current == 'i')
-			printf("%d", va_arg(args, int));
-		else if (current == 'f')
-			printf("%f", (float)va_arg(args, double));
-		else if (current == 's')
+		should_print_comma = 1;
+
+		switch (current)
 		{
+		case 'c':
+			printf("%c", (char)va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", (float)va_arg(args, double));
+			break;
+		case 's':
 			str = va_arg(args, const char *);
 			printf("%s", str ? str : "(nil)");
+			break;
+		default:
+			should_print_comma = 0;
 		}
 
 		i++;
 
-		if (format[i] && (current == 'c' || current == 'i' ||
-						  current == 'f' || current == 's'))
-		{
+		if (format[i] && should_print_comma)
 			printf(", ");
-		}
 	}
 
 	printf("\n");
