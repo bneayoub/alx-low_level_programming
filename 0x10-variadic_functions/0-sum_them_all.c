8492fc1fd9692331584b4b3546c6ef5b3b780709
bneayoub <bne.ayoub@gmail.com>
2023-04-20 18:17:03 +0000

    sum of args

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,6 +15,8 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
+	if (n == 0)
+		return (0);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdarg.h>
 /**
  * File: header for variadic functions
  *
