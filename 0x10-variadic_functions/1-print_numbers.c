8ac6c46fea82e4985674a62c78a2c334cd60e60d
bneayoub <bne.ayoub@gmail.com>
2023-04-20 18:44:34 +0000

    fixed null separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		printf("%d", num);
 
-		if (i < n - 1)
+		if (i < n - 1 && separator)
 		{
 			printf("%s", separator);
 		}
