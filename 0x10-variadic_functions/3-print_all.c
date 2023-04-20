9ac5fd57c6c893496e79fd7f115a98ab06bcedc6
bneayoub <bne.ayoub@gmail.com>
2023-04-20 19:04:14 +0000

    print ALL

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - print args c,s,i,e,f... separated by a comma
+ *
+ * @format: arg types in string
+ *
+ * Return: void and prints all
+ */
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	int i = 0;
+	const char *str;
+	char current;
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		current = format[i];
+		if (current == 'c')
+			printf("%c", (char)va_arg(args, int));
+		else if (current == 'i')
+			printf("%d", va_arg(args, int));
+		else if (current == 'f')
+			printf("%f", (float)va_arg(args, double));
+		else if (current == 's')
+		{
+			str = va_arg(args, const char *);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+		}
+		if (format[i + 1] && (current == 'c' || current == 'i' ||
+		current == 'f' || current == 's'))
+			printf(", ");
+		i++;
+	}
+
+	printf("\n");
+	va_end(args);
+}
