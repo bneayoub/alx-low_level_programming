8433e4d9556fd91250eaac0f3d23c54ffd2fc889
bneayoub <bne.ayoub@gmail.com>
2023-04-20 18:08:44 +0000

    sum of args

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - sum all arguments together
+ *
+ * @n: number of args
+ *
+ * Return: total of addition
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/**
+ * File: header for variadic functions
+ *
+ * Description: contains all prototypes needed
+*/
+int sum_them_all(const unsigned int n, ...);
+
+#endif
