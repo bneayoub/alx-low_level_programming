67b471dcc490934a69ba98ffa1747d58852f8a93
bneayoub <bne.ayoub@gmail.com>
2023-04-21 01:33:46 +0000

    fixed.h files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,11 @@
 #ifndef VARIADIC_FUNC
 #define VARIADIC_FUNC
 
-
-/* my headers */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-
-
-
-
-
-/* my prototypes */
+/* All prototypes */
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
