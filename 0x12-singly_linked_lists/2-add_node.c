e8272482436aae1664f1db61d1c68bca04c1bbfb
bneayoub <bne.ayoub@gmail.com>
2023-04-27 11:29:42 +0100

    Adding a node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node - append a node to the start of the linked list
+ * @head: ** head of the list
+ * @str: str to append
+ *
+ * Return: Address of the added element
+ * NULL otherwise
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+	
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - linked list singly
@@ -19,5 +20,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
