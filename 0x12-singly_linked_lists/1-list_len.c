28dba51e82c86fc52b37652c8abb650e2b9fb52a
bneayoub <bne.ayoub@gmail.com>
2023-04-27 11:24:25 +0100

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * list_len - counts numbers of nodes in list_t
+ *
+ * @h: head of the linked list
+ *
+ * Return: number of nodes in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes_count;
+
+	nodes_count = 0;
+
+	while (h)
+	{
+		nodes_count++;
+		h = h->next;
+	}
+
+	return (nodes_count);
+}
