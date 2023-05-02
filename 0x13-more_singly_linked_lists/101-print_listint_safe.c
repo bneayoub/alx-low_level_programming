8d35fe936ff814d75246d4181c41226e0a51672a
bneayoub <bne.ayoub@gmail.com>
2023-05-02 10:52:38 +0100

    debuggin task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,35 +1,84 @@
 #include "lists.h"
+
+/**
+ * looped_listint_len - Counts the number of unique nodes in a looped linked list
+ *
+ * @head: head of the listint_t to check.
+ *
+ * Return: 0 or nodes count
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				nodes++;
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			slow = slow->next;
+			while (slow != fast)
+			{
+				nodes++;
+				slow = slow->next;
+			}
+
+			return (nodes);
+		}
+
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (0);
+}
+
 /**
  * print_listint_safe - prints a listint_t linked list
  *
  * @head: head of the list
+ *
  * Return: the node count
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *tmp;
-	size_t count = 0;
+	size_t nodes, index = 0;
 
-	if (head == NULL)
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
 	{
-		exit(98);
-	}
-
-	current = head;
-
-	while (current != NULL)
-	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-		tmp = current;
-		current = current->next;
-
-		if (tmp < current)
+		for (; head != NULL; nodes++)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
 
-	return (count);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,6 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+size_t looped_listint_len(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
