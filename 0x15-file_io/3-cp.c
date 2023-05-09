b433adc4638c38309e7ef8a61c3c143530753673
bneayoub <bne.ayoub@gmail.com>
2023-05-09 01:14:51 +0100

    debugging cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,76 +1,118 @@
 #include "main.h"
+
 /**
- * main - chack the code
+ * check97 - checks 97
  *
  * @argc: number of arguments
- * @argv: args list
  *
- * Return: 0, 97, 98, 99, 100 depending on success
- * or error due to a type of errors
+ * Return: void
  */
-int main(int argc, char *argv[])
+void check97(int argc)
 {
-	int file_from = open(argv[1], O_RDONLY), file_to, r, w;
-	char *buf = allocate_buffer(argv[2]);
-
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_file_from file_file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	r = read(file_from, buf, 1024);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	do {
-		if (file_from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read file_from file %s\n", argv[1]);
-			free(buf);
-			exit(98);
-		}
-		w = write(file_to, buf, r);
-		if (file_to == -1 || w == -1)
-		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't write file_to %s\n", argv[2]);
-			free(buf);
-			exit(99);
-		}
-		r = read(file_from, buf, 1024);
-		file_to = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
-	free(buf);
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-	return (0);
 }
 
 /**
- * allocate_buffer - allocate 1024 bytes for a buffer
+ * check98 - checks 98
  *
- * @file: file that we are creating buffer for
+ * @check: checks if true of false
+ * @file: file_from name
+ * @file_from: file descriptor of file_from, or -1
+ * @file_to: file descriptor of file_to, or -1
  *
- * Return: buffer
+ * Return: void
  */
-char *allocate_buffer(char *file)
+void check98(ssize_t check, char *file, int file_from, int file_to)
 {
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
+	if (check == -1)
 	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (file_from != -1)
+			close(file_from);
+		if (file_to != -1)
+			close(file_to);
+		exit(98);
+	}
+}
+
+/**
+ * check99 - check 99
+ *
+ * @check: checks if true of false
+ * @file: file_to name
+ * @file_from: file descriptor of file_from, or -1
+ * @file_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check99(ssize_t check, char *file, int file_from, int file_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (file_from != -1)
+			close(file_from);
+		if (file_to != -1)
+			close(file_to);
 		exit(99);
 	}
+}
 
-	return (buffer);
+/**
+ * check100 - 100
+ *
+ * @check: checks if true or false
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check100(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - check the code
+ *
+ * @argc: number of args
+ * @argv: args list
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, close_to, close_from;
+	ssize_t lenr, lenw;
+	char buffer[1024];
+	mode_t file_perm;
+
+	check97(argc);
+	file_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)file_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)file_to, argv[2], file_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
+	{
+		lenr = read(file_from, buffer, 1024);
+		check98(lenr, argv[1], file_from, file_to);
+		lenw = write(file_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check99(lenw, argv[2], file_from, file_to);
+	}
+	close_to = close(file_to);
+	close_from = close(file_from);
+	check100(close_to, file_to);
+	check100(close_from, file_from);
+	return (0);
 }
