bef8357d5a73cfd1153fe8c2094d8fe412196144
bneayoub <bne.ayoub@gmail.com>
2023-05-09 01:24:20 +0100

    Elf header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
