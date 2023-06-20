45a37f69ffa489b511487b45ca0993e402112161
bneayoub <bne.ayoub@gmail.com>
2023-06-19 19:25:42 -0700

    chmod

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://raw.githubusercontent.com/bneayoub/alx-low_level_programming/master/0x18-dynamic_libraries/iwin.so
+wget -P /tmp wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/master/0x18-dynamic_libraries/iwin.so
 export LD_PRELOAD=/tmp/iwin.so
