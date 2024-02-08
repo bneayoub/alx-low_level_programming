3bc55351435f319d4304f313567da56947912aeb
bneayoub <bne.ayoub@gmail.com>
2024-02-08 22:50:26 +0100

    Refactor jump_search function to improve readability and performance

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,75 +1,39 @@
 #include "search_algos.h"
 
 /**
- * min - find min of a & b
- * @a: The first value
- * @b: The second value
- * Return: The minimum of the two values
- */
-size_t min(size_t a, size_t b)
-{
-	return ((a < b) ? a : b);
-}
-
-/**
- * _linear_search - Perform linear search on a subarray
- * @array: Pointer to the first element of the subarray
- * @start: Starting index of subarray
- * @end: Ending index of subarray
- * @value: Value to search for
- * Return: The index where value is located or -1 if not found
- */
-int _linear_search(int *array, size_t start, size_t end, int value)
-{
-	size_t i;
-
-	for (i = start; i < end; ++i)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
-	return (-1);
-}
-
-/**
- * jump_search - Jump Search on a sorted array
- * @array: Pointer to the first element
- * @size: Number of elements
- * @value: The value to search for
- * Return: The index where value is located or -1
- */
+  * jump_search - Searches for a value in a sorted array
+  *               of integers using jump search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the first index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  *              Uses the square root of the array size as the jump step.
+  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t block_size = sqrt(size);
-	size_t prev = 0;
-	int idx;
+	size_t i, jump, step;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
+	step = sqrt(size);
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
+	}
 
-	while (array[min(block_size, size) - 1] < value)
-	{
-		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-		prev = block_size;
-		block_size += sqrt(size);
-		if (prev >= size)
-			return (-1);
-	}
-	printf("Value found between indexes [%lu] and [%lu]\n",
-	prev, min(block_size, size) - 1);
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
-	idx = _linear_search(array, prev, min(block_size, size), value);
-	if (idx != -1)
-	{
-		printf("Found %d at index: %d\n", value, idx);
-		return (idx);
-	}
-	else
-	{
-		printf("Found %d at index: -1\n", value);
-		return (-1);
-	}
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
 }
-
